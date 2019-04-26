f9067852f3ee600f12bdba8e2a8eb4bf5e119771
khaled <khlddev@gmail.com>
2019-04-26 10:45:02 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import Vue from 'vue';
 import AdminDashboard from './components/admin-dashboard.vue';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/ar'
+
+library.add(fab, far, fas)
+
+Vue.component('fa', FontAwesomeIcon)
+
+Vue.use(Element, { locale })
+
 
 const Components = {
 	AdminDashboard
